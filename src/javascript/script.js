5153a37be59790bdbb4b41edd81fc48487e1e389
Hello World <joaop.codes@gmail.com>
2024-07-22 15:53:54 -0300

    finalizando projeto.
    aplicando java script e finalizando responsividade.

---
+++ src/javascript/script.js
@@ -0,0 +1,23 @@
+var msg = document.getElementById("msg")
+var title = document.getElementById("title-msg")
+var ft = document.getElementsByClassName("baner")
+
+function home() {
+    title.innerHTML = "Um Mergulho no Futuro Distópico"
+    msg.innerHTML = "Imagine um futuro onde a alta tecnologia se mistura com o colapso social, onde mega corporações controlam tudo e a linha entre o homem e a máquina é tênue.Bem-vindo ao mundo Cyberpunk."
+}
+function page2() {
+    title.innerHTML = "O Que é Cyberpunk?"
+    msg.innerHTML = "Cyberpunk é um subgênero da ficção científica que se caracteriza por cenários urbanos futuristas, alta tecnologia, e uma sociedade decadente. Inspirado por obras literárias, filmes, e videogames, o cyberpunk explora temas como inteligência artificial, biotecnologia, e a luta pela sobrevivência em um ambiente governado por corporações opressivas."
+}
+function page3() {
+    title.innerHTML = "Características Principais:"
+    msg.innerHTML = "Tecnologia Avançada: Implantes cibernéticos, redes neurais, e realidade virtual fazem parte do cotidiano. Ambiente Urbano: Cidades superpovoadas, neon brilhando em cada esquina e arranha-céus imponentes. Corporatocracia: Mega corporações que possuem mais poder que governos, influenciando todos os aspectos da vida. Anti-Heróis: Personagens marginais que lutam contra o sistema, hackers e renegados em busca de justiça ou sobrevivência"
+}
+function page4() {
+    title.innerHTML = "Junte-se à Revolução "
+    msg.innerHTML = "Se você é fascinado pela interseção de tecnologia e humanidade, pelo desafio de combater um sistema corrupto, e pelas possibilidades infinitas do futuro, você está no lugar certo. Prepare-se para explorar, desafiar e reinventar a realidade com o espírito do Cyberpunk."
+}
+
+
+
